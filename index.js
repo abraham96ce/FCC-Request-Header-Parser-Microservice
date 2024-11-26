ea871de250d988f34f793d6516f5107437e2c580
abraham96ce <abraham_96ce@hotmail.com>
2024-11-26 10:26:49 +0000

    Initial commit

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+// Ruta de la API que devuelve información sobre la solicitud del cliente
+app.get('/api/whoami', function (req, res) {
+  res.json({ 
+    ipaddress: req.socket.remoteAddress,  // Dirección IP del cliente
+    language: req.headers['accept-language'], // Idioma preferido
+    software: req.headers['user-agent'] // Información sobre el navegador y sistema operativo
+   });
 });
 
 // listen for requests :)
